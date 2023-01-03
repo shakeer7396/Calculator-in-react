c7c9a3370210384a1471297a3ad1d5cd1019919c
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2023-01-03 12:04:26 +0530

    calculator

--- calculator/src/Components/Home.jsx
+++ calculator/src/Components/Home.jsx
@@ -1,10 +1,67 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 const Home = () => {
+  const [result,setResult]=useState("");
+  const inputRef=useRef(null);
+
+  useEffect(()=>{
+    inputRef.current.focus();
+  },[])
+
+  const handleClick=(e)=>{
+    setResult(result.concat(e.target.name))
+  }
+
+  const handleClear=()=>{
+    setResult("")
+  }
+
+  const backspace=()=>{
+    setResult(result.slice(0,result.length-1))
+  }
+
+  const handleCalculate=()=>{
+    try{
+      setResult(eval(result).toString());
+    }
+    catch(err){
+      setResult('Error')
+    }
+  }
+
+
+
+
   return (
     <>
-    <div>Home Page</div>
     <h1>Calculator</h1>
+    <div className='calc-app' >
+      <form action="">
+        <input type="text" value={result} ref={inputRef} />
+      </form>
+
+      <div className="keypad">
+        <button id='clear' onClick={handleClear}>Clear</button>
+        <button id="backspace" onClick={backspace}>Back</button>
+        <button name='+' onClick={handleClick} >+</button>
+        <button name='7' onClick={handleClick} >7</button>
+        <button name='8' onClick={handleClick} >8</button>
+        <button name='9' onClick={handleClick} >9</button>
+        <button name='-' onClick={handleClick} >-</button>
+        <button name='4' onClick={handleClick} >4</button>
+        <button name='5' onClick={handleClick} >5</button>
+        <button name='6' onClick={handleClick} >6</button>
+        <button name='*' onClick={handleClick} >&#215;</button>
+        <button name='1' onClick={handleClick} >1</button>
+        <button name='2' onClick={handleClick} >2</button>
+        <button name='3' onClick={handleClick} >3</button>
+        <button name='/' onClick={handleClick} >/</button>
+        <button name='.' onClick={handleClick} >.</button>
+        <button name='0' onClick={handleClick} >0</button>
+        <button name='=' id='result' onClick={handleCalculate} >=</button>
+
+      </div>
+    </div>
     </>
   )
 }
